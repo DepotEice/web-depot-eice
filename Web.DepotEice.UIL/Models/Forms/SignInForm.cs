f1974df45e712a6d84fe1cc8985d14f222b8c42b
Soultan Hatsijev <soultan_98@hotmail.com>
2022-11-03 22:02:28 +0100

    Fixed few mistakes and added bootstrap spinner on loading

--- Web.DepotEice.UIL/Models/Forms/SignInForm.cs
+++ Web.DepotEice.UIL/Models/Forms/SignInForm.cs
@@ -4,10 +4,11 @@ namespace Web.DepotEice.UIL.Models.Forms
 {
     public class SignInForm
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez entrer une adresse email valide !")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez entrer une adresse email")]
+        [EmailAddress(ErrorMessage = "Veuillez entrer une adresse email valide")]
         public string Email { get; set; } = string.Empty;
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez entrer votre mot de passe !")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Veuillez entrer votre mot de passe")]
         public string Password { get; set; } = string.Empty;
     }
 }

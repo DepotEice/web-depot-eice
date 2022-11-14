d64b096cd39cb974c9d523b3e697d4ea6aa5aeb9
Soultan Hatsijev <soultan_98@hotmail.com>
2022-11-14 13:04:08 +0100

    Article create page

---
+++ Web.DepotEice.BLL/Services/ArticleService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Web.DepotEice.BLL.IServices;
+using Web.DepotEice.BLL.Models;
+
+namespace Web.DepotEice.BLL.Services
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ArticleService()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://localhost:7205/api/"),
+            };
+            _httpClient.DefaultRequestHeaders.Accept
+                .Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<ArticleModel?> GetArticleAsync(int id)
+        {
+            if (id == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"Articles/{id}");
+
+            response.EnsureSuccessStatusCode();
+
+            ArticleModel? article = await response.Content.ReadFromJsonAsync<ArticleModel>();
+
+            return article;
+        }
+
+        public async Task<IEnumerable<ArticleModel>> GetArticlesAsync()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("Articles");
+
+            response.EnsureSuccessStatusCode();
+
+            IEnumerable<ArticleModel>? articles = await response.Content.ReadFromJsonAsync<IEnumerable<ArticleModel>>();
+
+            if (articles is null)
+            {
+                return Enumerable.Empty<ArticleModel>();
+            }
+
+            return articles;
+        }
+    }
+}
--- Web.DepotEice.UIL/Client/Program.cs
+++ Web.DepotEice.UIL/Client/Program.cs
@@ -3,6 +3,8 @@ using Blazored.LocalStorage;
 using Blazored.SessionStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+using Syncfusion.Licensing;
 using Web.DepotEice.BLL.IServices;
 using Web.DepotEice.BLL.Services;
 using Web.DepotEice.UIL.Managers;
@@ -13,6 +15,9 @@ namespace Company.WebApplication1
     {
         public static async Task Main(string[] args)
         {
+            SyncfusionLicenseProvider
+                .RegisterLicense("NzU5NDEyQDMyMzAyZTMzMmUzMGJFdTU3bHV2S01SY1hzVnpxcmJ0djBpN29TMlpCM1hnbk53blJ3a3BCdDQ9");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<Web.DepotEice.UIL.App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
@@ -21,6 +26,10 @@ namespace Company.WebApplication1
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             builder.Services.AddBlazoredSessionStorage();
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddSyncfusionBlazor(options =>
+            {
+                options.IgnoreScriptIsolation = true;
+            });
 
             builder.Services.AddScoped(sp => new HttpClient
             {
@@ -31,6 +40,7 @@ namespace Company.WebApplication1
             builder.Services.AddScoped<IUserTokenService, UserTokenService>();
             builder.Services.AddScoped<IUserService, UserService>();
             builder.Services.AddScoped<IOpeningHoursService, OpeningHoursService>();
+            builder.Services.AddScoped<IArticleService, ArticleService>();
 
             builder.Services.AddScoped<UserManager>();
 

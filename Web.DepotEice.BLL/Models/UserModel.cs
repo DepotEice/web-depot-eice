25df1e66378b816bbbca71c1c5edd75ff285c4af
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-01 19:37:03 +0200

    fix: retrieve user data

--- Web.DepotEice.BLL/Models/UserModel.cs
+++ Web.DepotEice.BLL/Models/UserModel.cs
@@ -11,9 +11,12 @@ namespace Web.DepotEice.BLL.Models
         public string Id { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public bool EmailConfirmed { get; set; }
-        public string FirstName { get; set; } = string.Empty; 
+        public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string Gender { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
+        public string MobileNumber { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
         public bool IsActive { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }

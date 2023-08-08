cf3604c4123713ed02823a4e09ba6571fd762992
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-08 22:21:53 +0200

    feat: added data annotation

--- Web.DepotEice.UIL/Models/Forms/AddressForm.cs
+++ Web.DepotEice.UIL/Models/Forms/AddressForm.cs
@@ -7,14 +7,31 @@ namespace Web.DepotEice.UIL.Models.Forms
         public int Id { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "La rue est requise !")]
+        [MaxLength(150, ErrorMessage = "La rue doit faire au plus 150 caractères !")]
         public string Street { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le numéro de rue est requis !")]
+        [MaxLength(15, ErrorMessage = "Le numéro de rue doit faire au plus 15 caractères !")]
         public string HouseNumber { get; set; } = string.Empty;
+
+        [MaxLength(15, ErrorMessage = "Le numéro d'appartement doit faire au plus 15 caractères !")]
         public string? Appartment { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La ville est requise !")]
+        [MaxLength(100, ErrorMessage = "La ville doit faire au plus 100 caractères !")]
         public string City { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le code postal est requis !")]
+        [MaxLength(100, ErrorMessage = "Le code postal doit faire au plus 100 caractères !")]
         public string State { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le code postal est requis !")]
+        [MaxLength(15, ErrorMessage = "Le code postal doit faire au plus 15 caractères !")]
         public string Zipcode { get; set; } = string.Empty;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Le pays est requis !")]
+        [MaxLength(100, ErrorMessage = "Le pays doit faire au plus 100 caractères !")]
         public string Country { get; set; } = string.Empty;
-        public bool IsPrimary { get; set; }
-        public bool Editable { get; set; } = true;
+        public bool IsPrimary { get; set; } = false;
     }
 }

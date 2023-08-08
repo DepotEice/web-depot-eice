f890c209e0bc1995cbeafee76bc29757c03060a0
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-08 21:47:57 +0200

    feat: implemented validation

--- Web.DepotEice.UIL/Models/Forms/AddressForm.cs
+++ Web.DepotEice.UIL/Models/Forms/AddressForm.cs
@@ -1,12 +1,20 @@
-﻿namespace Web.DepotEice.UIL.Models.Forms
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.DepotEice.UIL.Models.Forms
 {
     public class AddressForm
     {
         public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "La rue est requise !")]
         public string Street { get; set; } = string.Empty;
-        public string Number { get; set; } = string.Empty;
-        public string? BoxNumber { get; set; }
-        public string ZipCode { get; set; } = string.Empty;
+        public string HouseNumber { get; set; } = string.Empty;
+        public string? Appartment { get; set; }
         public string City { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public string Zipcode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public bool IsPrimary { get; set; }
+        public bool Editable { get; set; } = true;
     }
 }
---
+++ Web.DepotEice.UIL/Profiles/AddressProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Web.DepotEice.BLL.Models;
+using Web.DepotEice.UIL.Models.Forms;
+
+namespace Web.DepotEice.UIL.Profiles
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile()
+        {
+            CreateMap<AddressModel, AddressForm>();
+            CreateMap<AddressForm, AddressCreateModel>();
+        }
+    }
+}

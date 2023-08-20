e7fc284236224bd97466c554c181d1076cb81ae6
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-20 23:14:47 +0200

    feat: implemented appointment update + delete

--- Web.DepotEice.BLL/IServices/IAppointmentService.cs
+++ Web.DepotEice.BLL/IServices/IAppointmentService.cs
@@ -7,10 +7,58 @@ using Web.DepotEice.BLL.Models;
 
 namespace Web.DepotEice.BLL.IServices
 {
+    /// <summary>
+    /// Appointment service interface
+    /// </summary>
     public interface IAppointmentService
     {
+        /// <summary>
+        /// Create an appointment by sending a POST request to the API
+        /// </summary>
+        /// <param name="appointmentCreate">The model to send to the API</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
+        /// <exception cref="ArgumentNullException"></exception>
         Task<ResultModel<AppointmentModel>> CreateAppointmentAsync(AppointmentCreateModel appointmentCreate);
+
+        /// <summary>
+        /// Update an appointment by sending a PUT request to the API
+        /// </summary>
+        /// <param name="id">The id of the appointment to update</param>
+        /// <param name="appointment">The appointment form</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        Task<ResultModel<AppointmentModel?>> UpdateAppointmentAsync(int id, AppointmentCreateModel appointment);
+
+        /// <summary>
+        /// Get all appointments by sending a GET request to the API
+        /// </summary>
+        /// <param name="date">The datetime</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="IEnumerable{AppointmentModel}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
         Task<ResultModel<IEnumerable<AppointmentModel>>> GetAppointmentsAsync(DateTime? date = null);
-        Task<AppointmentModel?> GetAppointmentAsync(int id);
+
+        /// <summary>
+        /// Get an appointment by sending a GET request to the API
+        /// </summary>
+        /// <param name="id">Id of the appointment</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        Task<ResultModel<AppointmentModel?>> GetAppointmentAsync(int id);
+
+        /// <summary>
+        /// Delete an appointment by sending a DELETE request to the API
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        Task<ResultModel<bool>> DeleteAppointmentAsync(int id);
     }
 }
--- Web.DepotEice.BLL/Services/AppointmentService.cs
+++ Web.DepotEice.BLL/Services/AppointmentService.cs
@@ -12,12 +12,22 @@ using Web.DepotEice.BLL.Models;
 
 namespace Web.DepotEice.BLL.Services
 {
+    /// <summary>
+    /// Appointment service to communicate with the API
+    /// </summary>
     public class AppointmentService : IAppointmentService
     {
         private readonly HttpClient _httpClient;
         private readonly ISyncLocalStorageService _localStorageService;
         private readonly ILogger<AppointmentService> _logger;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="localStorageService"></param>
+        /// <param name="httpClient"></param>
+        /// <param name="logger"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public AppointmentService(ISyncLocalStorageService localStorageService, HttpClient httpClient,
             ILogger<AppointmentService> logger)
         {
@@ -80,21 +90,100 @@ namespace Web.DepotEice.BLL.Services
             catch (Exception e)
             {
                 _logger.LogInformation($"{nameof(CreateAppointmentAsync)}: An exception was thrown, cannot " +
-                                                           $"read the result as json.\n{e.Message}");
+                    $"read the result as json.\n{e.Message}");
             }
 
             return result;
         }
 
-        public async Task<AppointmentModel?> GetAppointmentAsync(int id)
+        /// <summary>
+        /// Update an appointment by sending a PUT request to the API
+        /// </summary>
+        /// <param name="id">The id of the appointment to update</param>
+        /// <param name="appointment">The appointment form</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        public async Task<ResultModel<AppointmentModel?>> UpdateAppointmentAsync(int id, AppointmentCreateModel appointment)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync($"Appointments/{id}");
+            if (id == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
 
-            var result = await response.Content.ReadFromJsonAsync<AppointmentModel>();
+            if (appointment is null)
+            {
+                throw new ArgumentNullException(nameof(appointment));
+            }
+
+            HttpResponseMessage response = await _httpClient.PutAsJsonAsync($"Appointments/{id}", appointment);
+
+            ResultModel<AppointmentModel?> result = new()
+            {
+                Success = response.IsSuccessStatusCode,
+                Code = response.StatusCode,
+                Message = await response.Content.ReadAsStringAsync()
+            };
+
+            try
+            {
+                result.Data = await response.Content.ReadFromJsonAsync<AppointmentModel>();
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation($"{nameof(UpdateAppointmentAsync)}: An exception was thrown, cannot " +
+                                       $"read the result as json.\n{e.Message}");
+            }
 
             return result;
         }
 
+        /// <summary>
+        /// Get an appointment by sending a GET request to the API
+        /// </summary>
+        /// <param name="id">Id of the appointment</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public async Task<ResultModel<AppointmentModel?>> GetAppointmentAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+
+            HttpResponseMessage response = await _httpClient.GetAsync($"Appointments/{id}");
+
+            ResultModel<AppointmentModel?> result = new()
+            {
+                Code = response.StatusCode,
+                Success = response.IsSuccessStatusCode,
+                Message = await response.Content.ReadAsStringAsync()
+            };
+
+            try
+            {
+                result.Data = await response.Content.ReadFromJsonAsync<AppointmentModel>();
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation($"{nameof(GetAppointmentAsync)}: An exception was thrown, cannot " +
+                    $"read the result as json.\n{e.Message}");
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get all appointments by sending a GET request to the API
+        /// </summary>
+        /// <param name="date">The datetime</param>
+        /// <returns>
+        /// <see cref="ResultModel{T}"/> where T is <see cref="IEnumerable{AppointmentModel}"/> where T is <see cref="AppointmentModel"/>
+        /// </returns>
         public async Task<ResultModel<IEnumerable<AppointmentModel>>> GetAppointmentsAsync(DateTime? date = null)
         {
             string queryUri = "Appointments";
@@ -121,6 +210,41 @@ namespace Web.DepotEice.BLL.Services
             {
                 _logger.LogInformation($"{nameof(GetAppointmentsAsync)}: An exception was thrown, cannot " +
                         $"read the result as json.\n{e.Message}");
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Delete an appointment by sending a DELETE request to the API
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public async Task<ResultModel<bool>> DeleteAppointmentAsync(int id)
+        {
+            if (id <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"Appointments/{id}");
+
+            ResultModel<bool> result = new()
+            {
+                Success = response.IsSuccessStatusCode,
+                Code = response.StatusCode,
+                Message = await response.Content.ReadAsStringAsync()
+            };
+
+            try
+            {
+                result.Data = await response.Content.ReadFromJsonAsync<bool>();
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation($"{nameof(DeleteAppointmentAsync)}: An exception was thrown, cannot " +
+                                       $"read the result as json.\n{e.Message}");
             }
 
             return result;

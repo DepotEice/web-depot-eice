e3e53e679edc813e67c9ccf30d2544da44a77ee7
Soultan Hatsijev <soultan_98@hotmail.com>
2023-08-31 20:42:56 +0200

    refactor: change the sf scheduler for the radzen one

--- Web.DepotEice.UIL/Models/ModuleDisplayModel.cs
+++ Web.DepotEice.UIL/Models/ModuleDisplayModel.cs
@@ -6,5 +6,16 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string TeacherFullName { get; set; } = string.Empty;
+
+        public string RGBColor { get; private set; }
+
+        public ModuleDisplayModel()
+        {
+            int red = new Random().Next(0, 200);
+            int green = new Random().Next(0, 200);
+            int blue = new Random().Next(0, 200);
+
+            RGBColor = $"rgb({red},{green},{blue})";
+        }
     }
 }
--- Web.DepotEice.UIL/Models/ScheduleDisplayModel.cs
+++ Web.DepotEice.UIL/Models/ScheduleDisplayModel.cs
@@ -1,14 +1,27 @@
-﻿namespace Web.DepotEice.UIL.Models
+﻿using Syncfusion.Blazor.HeatMap;
+
+namespace Web.DepotEice.UIL.Models
 {
     public class ScheduleDisplayModel
     {
         public int Id { get; set; }
-        public string ModuleName { get; set; }
-        public string TeacherName { get; set; }
-        public string Title { get; set; }
-        public string Details { get; set; }
+        public string ModuleName { get; set; } = string.Empty;
+        public string TeacherName { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public string Details { get; set; }  = string.Empty;
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
+
+        public string RGBColor { get; private set; } = string.Empty;
+
+        public ScheduleDisplayModel()
+        {
+            int red = new Random().Next(0, 200);
+            int green = new Random().Next(0, 200);
+            int blue = new Random().Next(0, 200);
+
+            RGBColor = $"rgb({red},{green},{blue})";
+        }
 
         public string ScheduleDetailsUrl
         {
